5ac36776bfb7e07223d20794d60ef64c5bc4b886
ShoujiKanenobu <shoujikanenobu@gmail.com>
2019-10-12 15:14:59 -0700

    updated sql

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,24 +1,23 @@
-CREATE DATABASE myDB
+CREATE DATABASE myDB;
 
-CREATE TABLE users (
+CREATE TABLE "users" (
 	userid serial NOT NULL PRIMARY KEY,
 	username varchar (255) NOT NULL,
-	password varchar(255) NOT NULL
+	password varchar(255) NOT NULL,
 	online boolean NOT NULL
-)
+);
 
-CREATE TABLE posts (
+CREATE TABLE "posts" (
 	postid serial NOT NULL PRIMARY KEY,
 	message varchar(255) NOT NULL,
 	postTime TIMESTAMP NOT NULL,
 	userid integer NOT NULL references users(userid)
-)
+);
 
-CREATE TABLE friends(
+CREATE TABLE "friends" (
 	friendid serial NOT NULL PRIMARY KEY,
 	acceptTime TIMESTAMP NOT NULL,
 	user_first integer NOT NULL references users(userid)
 	user_second integer NOT NULL references users(userid)
 );
-
 
